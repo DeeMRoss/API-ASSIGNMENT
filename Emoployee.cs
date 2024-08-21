581341cef92fcb9f177d0fb972daf9f2dcc74557
DeeMRoss <deeross2345@gmail.com>
2024-08-21 11:23:16 -0500

    Deliverable

---
+++ Emoployee.cs
@@ -0,0 +1,6 @@
+public class Employee
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+     public string? MiddleName { get; set; }
+      public string? LastName { get; set; }}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<EmployeeDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+app.MapGet("/todoitems", async (EmployeeDb db) =>
+    await db.Employees.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (EmployeeDb db) =>
+    await db.Employees.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, EmployeeDb db) =>
+    await db.Employees.FindAsync(id)
+        is Employee todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+app.Run();
